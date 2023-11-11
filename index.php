14232f31c058de8b6a389271b79eb618c002c8ca
Alduinv <Lord.mt@outlook.com>
2023-11-11 23:21:31 +0300

    init

--- index.php
+++ index.php
@@ -11,21 +11,29 @@ include './inc/db_close.php';
 
 
 <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-    <div class="col-md-5 p-lg-5 mx-auto my-5">
+    <div class="col-md-5 p-lg-5 mx-auto ">
+
+    <img src="images/images.jpeg" alt="" >
       <h1 class="display-4 fw-normal">Punny headline</h1>
-      <p class="lead fw-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple’s marketing pages.</p>
+      <p class="lead fw-normal"> pages.</p>
+      <h3 id="#countdown"></h3>
+      <p class="lead fw-normal"> pages.</p>
       <a class="btn btn-outline-secondary" href="#">Coming soon</a>
     </div>
-  </div>
-<!--
-  <ul class="list-group list-group-flush">
+    <div class="container">
+    <ul class="list-group list-group-flush">
   <li class="list-group-item">An item</li>
   <li class="list-group-item">A second item</li>
   <li class="list-group-item">A third item</li>
   <li class="list-group-item">A fourth item</li>
   <li class="list-group-item">And a fifth one</li>
 </ul>
--->
+    </div>
+  </div>
+
+ 
+
+<div class="container">
 <div class="position-relative  text-center ">
     <div class="col-md-5 p-lg-5 mx-auto my-5">
     </div>
@@ -54,13 +62,46 @@ include './inc/db_close.php';
 
 
 
-<div class="row">
-<?php foreach($users as $user) : ?>
- <h1> <?php echo htmlspecialchars($user['firstName']) 
- . ' '. htmlspecialchars($user['lastName']) . '<br>' 
- . htmlspecialchars($user['email']);  ?> </h1>
+<div class="loader-con">
+<div id="loader">
+	<canvas id="circularLoader" width="200" height="200"></canvas>
+</div>
+</div>
 
-<?php endforeach; ?> 
+<!-- Button trigger modal -->
+<div class="d-grid gap-2 col-6 mx-auto my-5">
+<button type="button" id="winner" class="btn btn-primary" >
+choose winner
+</button>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="modal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalLabel">The winner is</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+
+      <div class="modal-body">
+      <?php foreach($users as $user) : ?>
+        <h1 class="display-3 text-center modal-title" id="modalLabel"> <?php echo htmlspecialchars($user['firstName']) 
+         . ' '. htmlspecialchars($user['lastName'])  ?> </h1>
+
+        <?php endforeach; ?> 
+      </div>
+     
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+<div class="row">
+
 
 
 <?php include_once './parts.footer.php'; ?>
